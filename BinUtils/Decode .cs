70f4bef629b602aaa11771d6b6496e22180239bb
Daniel <78626236+too-many-hats@users.noreply.github.com>
2024-01-21 18:31:56 +1100

    Signed word decode with some tests

---
+++ BinUtils/Decode .cs
@@ -0,0 +1,19 @@
+﻿namespace BinUtils;
+
+public static class Decode
+{
+    public static long SignedNumber(ulong word)
+    {
+        if ((word & Constants.SignBitMask) == 0)
+        {
+            return (long)(word & Constants.MagnitudeBitMask);
+        }
+
+        var magnitude = (~(word & Constants.MagnitudeBitMask) & Constants.MagnitudeBitMask);
+
+        // sign extend and make negative
+        return ((long)magnitude) * -1;
+    }
+
+    //NOTE there is no decoder for unsigned words, because the value doesn't change. There is also no decoder for instructions, use a disassembler.
+}
---
+++ BinUtilsTests/DataDecodeTests.cs
@@ -0,0 +1,17 @@
+﻿using BinUtils;
+using Xunit;
+
+namespace BinUtilsTests;
+
+public class DataDecodeTests
+{
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(68719476734, -1)]
+    public void CanDecodeSignedWord(ulong value, long expected)
+    {
+        var result = Decode.SignedNumber(value);
+        Assert.Equal(expected, result);
+    }
+}
