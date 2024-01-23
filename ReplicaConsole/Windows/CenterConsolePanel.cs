abe1b8724c8fe20688602c828723cd111569114a
Daniel <78626236+too-many-hats@users.noreply.github.com>
2024-01-23 16:12:47 +1100

    Address poorly chosen naming convention

--- ReplicaConsole/Windows/CenterConsolePanel.cs
+++ ReplicaConsole/Windows/CenterConsolePanel.cs
@@ -53,14 +53,6 @@ public class CenterConsolePanel : IWindow
 
     }
 
-    private SDL.SDL_Rect indicatorSource = new()
-    {
-        h = 113,
-        w = 113,
-        x = 0,
-        y = 0,
-    };
-
     public void Update()
     {
         // Clears the current render surface.
@@ -69,26 +61,97 @@ public class CenterConsolePanel : IWindow
             // Console.WriteLine($"There was an issue with clearing the render surface. {SDL.SDL_GetError()}");
         }
 
-        RenderIndicatorCollection(Console.AIndicators, 0, 150, true, 3);
-        RenderIndicatorCollection(Console.QIndicators, 53 * 36, 0, true, 3);
-        RenderIndicatorCollection(Console.XIndicators, 53 * 36, 300, true, 3);
-        RenderIndicatorCollection(Console.MCRIndicators, 53 * 36, 450, true, 3);
-        RenderIndicatorCollection(Console.UAKIndicators, 53 * 42, 450, true, 3);
-        RenderIndicatorCollection(Console.VAKIndicators, 53 * 57, 450, true, 3);
-        RenderIndicatorCollection(Console.PAKIndicators, 53 * 36, 600, true, 3);
-        RenderIndicatorCollection(Console.SARIndicators, 53 * 57, 600, true, 3);
-        RenderIndicatorCollection([Console.AscDelAddIndicator, Console.AscSpSubtIndicator, Console.AscOverflowIndicator, Console.AscALIndicator, Console.AscARIndicator, Console.AscBIndicator, Console.AscCIndicator, Console.AscDIndicator, Console.AscEIndicator], 0, 0, true, 3);
-        RenderIndicatorCollection([Console.InitArithSequenceLogIndicator, Console.InitArithSequenceA_1Indicator, Console.InitArithSequenceSPIndicator, Console.InitArithSequenceA1Indicator, Console.InitArithSequenceQLIndicator, Console.InitArithSequenceDivIndicator, Console.InitArithSequenceMultIndicator, Console.InitArithSequenceSEQIndicator, Console.InitArithSequenceStepIndicator, Console.InitArithSequenceCaseIndicator, Console.InitArithSequenceCKIndicator, Console.InitArithSequenceCKIIndicator, Console.InitArithSequenceRestXIndicator, Console.InitArithSequenceMultiStepIndicator, Console.InitArithSequenceExtSeoIndicator], 53 * 13, 0, true, 3);
-        RenderIndicatorCollection([Console.StopTapeIndicator, Console.SccFaultIndicator, Console.MctFaultIndicator, Console.DivFaultIndicator, Console.AZeroIndicator, Console.TapeFeedIndicator, Console.Rsc75Indicator, Console.RscHoldRptIndicator, Console.RscJumpTermIndicator, Console.RscInitRptIndicator, Console.RscInitTestIndicator, Console.RscEndRptIndicator, Console.RscDelayTestIndicator, Console.RscAdvAddIndicator, Console.SccInitReadIndicator, Console.SccInitWriteIndicator, Console.SccInitIw0_14Indicator, Console.SccInitIw15_29Indicator, Console.SccReadQIndicator, Console.SccWriteAorQIndicator, Console.SccClearAIndicator], 0, 300, true, 3);
-        RenderIndicatorCollection([Console.MasterClockCSSIIndicator, Console.MasterClockCSSIIIndicator, Console.MasterClockCRCIIndicator, Console.MasterClockCRCIIIndicator], 0, 600, true, 3);
-        RenderIndicatorCollection([Console.PdcHpcIndicator, Console.PdcTwcIndicator, Console.PdcWaitInternalIndicator, Console.PdcWaitExternalIndicator, Console.PdcWaitRscIndicator, Console.PdcStop], 53 * 8, 600, true, 3);
-        RenderIndicatorCollection(Console.MpdIndicators, 53 * 18, 600, true, 3);
-        RenderIndicatorCollection([Console.SctA, Console.SctQ, Console.SctMD, Console.SctMcs0, Console.SctMcs1], 53 * 16, 450, true, 3);
-        RenderIndicatorCollection(Console.SkTranslatorIndicators, 0, 450, true, 3);
-        RenderIndicatorCollection(Console.JTranslatorIndicators, 53 * 8, 450, true, 3);
-        RenderIndicatorCollection(Console.MainPulseTranslatorIndicators, 53 * 24, 600, true, 3);
-        RenderIndicatorCollection([Console.Halt, Console.Interrupt], 53 * 53, 600, true, 1);
-        //TODO: Add all the other register and flip flop indicators.
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.AIndicators, 0, 150, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.QIndicators, 53 * 36, 0, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.XIndicators, 53 * 36, 300, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.MCRIndicators, 53 * 36, 450, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.UAKIndicators, 53 * 42, 450, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.VAKIndicators, 53 * 57, 450, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.PAKIndicators, 53 * 36, 600, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.SARIndicators, 53 * 57, 600, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, 
+            [
+                Console.AscDelAdd, 
+                Console.AscSpSubt, 
+                Console.AscOverflow, 
+                Console.AscAL, 
+                Console.AscAR, 
+                Console.AscB, 
+                Console.AscC, 
+                Console.AscD, 
+                Console.AscE
+            ], 0, 0, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, 
+            [
+                Console.InitArithSequenceLog, 
+                Console.InitArithSequenceA_1, 
+                Console.InitArithSequenceSP, 
+                Console.InitArithSequenceA1, 
+                Console.InitArithSequenceQL, 
+                Console.InitArithSequenceDiv, 
+                Console.InitArithSequenceMult, 
+                Console.InitArithSequenceSEQ, 
+                Console.InitArithSequenceStep, 
+                Console.InitArithSequenceCase, 
+                Console.InitArithSequenceCKI, 
+                Console.InitArithSequenceCKII, 
+                Console.InitArithSequenceRestX, 
+                Console.InitArithSequenceMultiStep, 
+                Console.InitArithSequenceExtSeq
+            ], 53 * 13, 0, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, 
+            [
+                Console.StopTape, 
+                Console.SccFault, 
+                Console.MctFault, 
+                Console.DivFault, 
+                Console.AZero, 
+                Console.TapeFeed,
+                Console.Rsc75,
+                Console.RscHoldRpt, 
+                Console.RscJumpTerm,
+                Console.RscInitRpt, 
+                Console.RscInitTest,
+                Console.RscEndRpt,
+                Console.RscDelayTest,
+                Console.RscAdvAdd,
+                Console.SccInitRead,
+                Console.SccInitWrite,
+                Console.SccInitIw0_14,
+                Console.SccInitIw15_29,
+                Console.SccReadQ,
+                Console.SccWriteAorQ, 
+                Console.SccClearA
+            ], 0, 300, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, 
+            [
+                Console.MasterClockCSSI,
+                Console.MasterClockCSSII,
+                Console.MasterClockCRCI,
+                Console.MasterClockCRCII
+            ], 0, 600, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, 
+            [
+                Console.PdcHpc,
+                Console.PdcTwc,
+                Console.PdcWaitInternal,
+                Console.PdcWaitExternal,
+                Console.PdcWaitRsc,
+                Console.PdcStop
+            ], 53 * 8, 600, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.MpdIndicators, 53 * 18, 600, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, 
+            [
+                Console.SctA,
+                Console.SctQ,
+                Console.SctMD,
+                Console.SctMcs0,
+                Console.SctMcs1
+            ], 53 * 16, 450, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.SkTranslatorIndicators, 0, 450, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.JTranslatorIndicators, 53 * 8, 450, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, Console.MainPulseTranslatorIndicators, 53 * 24, 600, true, 3);
+        ConsoleIndicatorsCommon.RenderIndicators(Renderer, ARegisterTextures, [Console.Halt, Console.Interrupt], 53 * 24, 600, true, 3);
 
         // Switches out the currently presented render surface with the one we just did work on.
         SDL.SDL_RenderPresent(Renderer);
@@ -97,46 +160,5 @@ public class CenterConsolePanel : IWindow
     public void Close()
     {
         SDL.SDL_DestroyRenderer(Renderer);
-    }
-
-    private void RenderIndicatorCollection(Indicator[] indicators, int x, int y, bool splitInGroupsOfThree, int splitOffsetLeft)
-    {
-        const int indicatorDiameter = 43;
-        const int indicatorWidthAndMargin = 53;
-
-        var yInternal = y;
-
-        if (indicators[0].HasHighAndLowLight is false) // when an indicator is only single row, always put the indicator on the bottom row.
-        {
-            yInternal = yInternal + indicatorWidthAndMargin;
-        }
-
-        for (int i = 0; i < indicators.Length; i++)
-        {
-            var indicator = indicators[i];
-
-            var destRect = new SDL.SDL_Rect()// where to place the indictor on the window.
-            {
-                h = indicatorDiameter,
-                w = indicatorDiameter,
-                x = x + i * indicatorWidthAndMargin,
-                y = yInternal,
-            };
-
-            // render the top indicator first
-            var totalCyclesIndicatorOn = indicator.SumIntensityRecordedFrames();
-            SDL.SDL_SetTextureAlphaMod(ARegisterTextures[i], (byte)(totalCyclesIndicatorOn / 133.33));
-       //     Debug.WriteLine("Alpha: " + (byte)(totalCyclesIndicatorOn / 266.66));
-            SDL.SDL_RenderCopy(Renderer, ARegisterTextures[i], ref indicatorSource, ref destRect);
-
-            if (indicator.HasHighAndLowLight)
-            {
-                // then render the bottom indicator if it exists.
-                destRect.y += indicatorWidthAndMargin;
-                SDL.SDL_SetTextureAlphaMod(ARegisterTextures[i + 1], (byte)((34000 - totalCyclesIndicatorOn) / 133.33));
-                SDL.SDL_RenderCopy(Renderer, ARegisterTextures[i + 1], ref indicatorSource, ref destRect);
-            }
-        }
-
     }
 }
---
+++ ReplicaConsole/Windows/ConsoleIndicatorsCommon.cs
@@ -0,0 +1,57 @@
+﻿using Emulator.Devices.Computer;
+using SDL2;
+
+namespace ReplicaConsole.Windows;
+
+public static class ConsoleIndicatorsCommon
+{
+    // the area of the image which we are pulling the indicator texture out of.
+    private static SDL.SDL_Rect indicatorSource = new()
+    {
+        h = 113,
+        w = 113,
+        x = 0,
+        y = 0,
+    };
+
+    public static void RenderIndicators(nint renderer, List<nint> indicatorTextures, Indicator[] indicators, int x, int y, bool splitInGroupsOfThree, int splitOffsetLeft)
+    {
+        const int indicatorDiameter = 43;
+        const int indicatorWidthAndMargin = 53;
+
+        var yInternal = y;
+
+        if (indicators[0].HasHighAndLowLight is false) // when an indicator is only single row, always put the indicator on the bottom row.
+        {
+            yInternal = yInternal + indicatorWidthAndMargin;
+        }
+
+        for (int i = 0; i < indicators.Length; i++)
+        {
+            var indicator = indicators[i];
+
+            var destRect = new SDL.SDL_Rect()// where to place the indictor on the window.
+            {
+                h = indicatorDiameter,
+                w = indicatorDiameter,
+                x = x + i * indicatorWidthAndMargin,
+                y = yInternal,
+            };
+
+            // render the top indicator first
+            var totalCyclesIndicatorOn = indicator.SumIntensityRecordedFrames();
+            SDL.SDL_SetTextureAlphaMod(indicatorTextures[i], (byte)(totalCyclesIndicatorOn / 133.33));
+            //     Debug.WriteLine("Alpha: " + (byte)(totalCyclesIndicatorOn / 266.66));
+            SDL.SDL_RenderCopy(renderer, indicatorTextures[i], ref indicatorSource, ref destRect);
+
+            if (indicator.HasHighAndLowLight)
+            {
+                // then render the bottom indicator if it exists.
+                destRect.y += indicatorWidthAndMargin;
+                SDL.SDL_SetTextureAlphaMod(indicatorTextures[i + 1], (byte)((34000 - totalCyclesIndicatorOn) / 133.33));
+                SDL.SDL_RenderCopy(renderer, indicatorTextures[i + 1], ref indicatorSource, ref destRect);
+            }
+        }
+
+    }
+}
